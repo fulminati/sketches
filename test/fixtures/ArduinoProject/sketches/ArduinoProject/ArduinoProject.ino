34f62d5e83f4c453be7d16a425513a2aef6fe23c
Francesco Bianco <info.francescobianco@gmail.com>
2019-11-20 19:49:32 +0100

    Release new version

--- test/fixtures/ArduinoProject/sketches/ArduinoProject/ArduinoProject.ino
+++ test/fixtures/ArduinoProject/sketches/ArduinoProject/ArduinoProject.ino
@@ -9,9 +9,15 @@
 #include "ArduinoProject.h"
 
 void setup() {
+  Serial.begin( @param('boudRate') );
   pinMode(ARDUINOPROJECT_OUTPUT, OUTPUT);
 }
 
 void loop() {
   digitalWrite(ARDUINOPROJECT_OUTPUT, HIGH);
+Serial.println("On");
+   delay(2000);
+   digitalWrite(ARDUINOPROJECT_OUTPUT, LOW);
+   Serial.println("Off");
+      delay(2000);
 }
