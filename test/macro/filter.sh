2c17c5206c0141cce7d5f93c42f9fe4e88b44713
Francesco Bianco <info.francescobianco@gmail.com>
2019-11-22 01:26:54 +0100

    Release new version

---
+++ test/macro/filter.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+
+temp=$(dirname $(realpath $0))/../temp
+env_file=${temp}/../../.env
+sketches=${temp}/../../sketches
+fixtures=${temp}/../fixtures
+
+rm -fr ${temp}
+mkdir -p ${temp}
+cd ${temp}
+
+cp -R ${fixtures}/ArduinoProject .
+cd ArduinoProject
+cp ${env_file} .
+
+#${sketches} install
+
+${sketches} apply-filter NanoOld --no-reset
